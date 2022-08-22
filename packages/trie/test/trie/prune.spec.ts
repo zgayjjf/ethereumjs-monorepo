c436e469b14c6e9c33a6c2e584ec4329f253de9e
ScottyPoi <scott.simpson@ethereum.org>
2022-08-21 20:02:27 -0400

    trie: Write prune test

--- packages/trie/test/trie/prune.spec.ts
+++ packages/trie/test/trie/prune.spec.ts
@@ -52,8 +52,48 @@ tape('Pruned trie tests', function (tester) {
     for (let i = 0; i < keys.length; i++) {
       //console.log(await trie.get(Buffer.from(keys[i], 'hex')))
     }
+    st.end()
+  })
 
-    // TODO
+  // TODO
+
+  it('should prune when keys are updated or deleted', async (st) => {
+    const trie = new Trie({ pruneTrie: true })
+    const keys: string[] = []
+    for (let i = 0; i < 100; i++) {
+      keys.push(i.toString(16))
+    }
+    const values: string[] = []
+    for (let i = 0; i < 1000; i++) {
+      let val = Math.floor(Math.random() * 16384)
+      while (values.includes(val.toString(16))) {
+        val = Math.floor(Math.random() * 16384)
+      }
+      values.push(val.toString(16))
+    }
+    for (let i = 0; i < keys.length; i++) {
+      const idx = i < 100 ? i : Math.floor(Math.random() * 100)
+      const key = keys[idx]
+      await trie.put(Buffer.from(key), Buffer.from(values[i]))
+    }
+    st.equals((<any>trie.db)._database.size, 20, 'DB size correct')
+
+    await trie.walkTrie(trie.root, (key) => {
+      st.deepEqual(key, trie.root, 'Walk tree passed')
+    })
+
+    for (let i = 0; i < 20; i++) {
+      await trie.del(Buffer.from(keys[i]))
+    }
+    st.equals((<any>trie.db)._database.size, 19, 'DB size correct')
+    try {
+      await trie.walkTrie(trie.root, (key) => {
+        st.deepEqual(key, trie.root, 'Walk tree passed')
+      })
+      st.pass('Prune test passed')
+    } catch {
+      st.fail('Prune test failed')
+    }
     st.end()
   })
 })
